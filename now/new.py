052e513ee0a6f4ee9d490a336bea7c0e896c05bf
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-03-28 23:24:39 +0300

    grading using if statements

--- now/new.py
+++ now/new.py
@@ -5,3 +5,24 @@ print(components[1])
 print(components[2])
 if len(components) > 3:
     print(components[3])
+
+mark = input("Enter your mark or X to exit\n")
+while mark.lower() != "x":
+    mark = int(mark)
+    if mark < 0 or mark > 100:
+        print("This grade is invalid")
+    elif mark < 31: 
+        print("Your grade is", "F9")
+    elif mark < 41:
+        print("Your grade is ","P8")
+    elif mark < 51:
+        print("Your grade is ","P7")
+    elif mark < 61:
+        print("Your grade is ","C6")
+    elif mark < 71:
+        print("Your grade is ","C5")
+
+    else :
+        print("Your grade is", "D1")
+    mark = input("Enter your mark or X to exit\n")
+print("Exiting, you entered X")
