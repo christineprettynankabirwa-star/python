c822e37d1c8197fdef4cec27a102665839a540f5
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-04-09 13:01:58 +0300

    checking for not prime number

--- prime.py
+++ prime.py
@@ -2,4 +2,10 @@ N = int(input("Enter any positive integer: "))
 
 # numbers <= 1 are not prime
 if N <= 1:
-    
+    print(f"{N} is not a prime number.")
+
+else:
+    is_prime = True
+
+    # check divisibility from 2 to N-1
+    for i in range(2, N):
