19aff4ac41cc82ac797d73c20f3958bf87b29b09
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-04-10 23:49:55 +0300

    iterate along the letters_list

--- now/ouryear.py
+++ now/ouryear.py
@@ -20,3 +20,17 @@ def compute_age(yob):
 age = compute_age(yob)   
 print(f"You are most welcome, {name}!")
 print(f"You are {age} years old now")
+
+
+letters_list = ['a','b','c','d']
+print(f"Letter in position 3 of list is {letters_list[2]}")
+print ("Items in the set")
+for letter in letters_set:
+    print(letter)
+print ("Items in the list")
+for letter in letters_list :
+    print(letter)
+try:
+    print(f"Letter in position 3 of list is {letters_set[2]}")
+except Exception as e:
+    print (e)
