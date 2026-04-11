36bf6e5974ae5bc55887d1c3641e05d02838fa4d
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-04-11 23:56:11 +0300

    final change : grade calculation

--- now/semassignment.py
+++ now/semassignment.py
@@ -43,3 +43,37 @@ print_menu()
 
 #Print menu for first time
 print_menu()
+
+#initialize the gradebook
+grade_book = dict()
+
+while True:
+    choice = input("\n--------------------\nEnter your choice\n")
+    
+    if choice == '1':
+        #Code to add a student. Teacher/User should supply admin_no, name and optionally marks for SST, Maths, science and Eng
+
+        print("Student added.") #Include details for added student and current number of students in grade book.
+    
+    elif choice == '2':
+        #Add Code to delete a student. Teacher/User admin_no for student to delete
+        print("Student deleted")
+    
+    elif choice == '3':
+        #Add Code give the following gradebook statistics
+        print("Mean marks for Maths: ")
+        print("Mode mark for Maths: ")
+        print("Modal frequency for Maths: ")
+        print("Lowest mark for Maths: ")
+        print("Highest mark for Maths: ")
+        #Repeat the above output for SST and Eng
+    
+    elif choice == 'm':
+        print_menu()
+    
+    elif choice == 'c':
+        os.system('cls') #Only for windows
+        
+    elif choice == 'q':
+        print('Bye bye')
+        break
