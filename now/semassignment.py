8e085a8566b842ca8c5f216c7ff13a9601e8742f
Nankabirwa Christine Pretty <christineprettynankabirwa@gmail.com>
2026-04-11 23:50:21 +0300

    created new file: now/semassignment.py here we are working on a gradebook

---
+++ now/semassignment.py
@@ -0,0 +1,45 @@
+
+import os
+your_reg_number = '2024/' #update your registration number
+your_name = 'John Doe' #update your name
+
+def print_menu():
+    print("--------------------Menu--------------------")
+    print("1 - Add student with marks")
+    print("2 - Delete student, given an admin_no")
+    print("3 - View statistics about the grades")
+    print("4 - View student grades")
+    print("5 - Edit student grades")
+    print("6 - Print Gradebook")
+    print("m - Print menu")
+    print("c - Clear Screen")
+    print("q - Quit system\n")
+
+print()
+print(f"Welcome to students Gradebook\nBy".upper())
+print(f"{your_name} - {your_reg_number}\n")
+import os
+your_reg_number = '2024/' #update your registration number
+your_name = 'John Doe' #update your name
+
+def print_menu():
+    print("--------------------Menu--------------------")
+    print("1 - Add student with marks")
+    print("2 - Delete student, given an admin_no")
+    print("3 - View statistics about the grades")
+    print("4 - View student grades")
+    print("5 - Edit student grades")
+    print("6 - Print Gradebook")
+    print("m - Print menu")
+    print("c - Clear Screen")
+    print("q - Quit system\n")
+
+print()
+print(f"Welcome to students Gradebook\nBy".upper())
+print(f"{your_name} - {your_reg_number}\n")
+
+#Print menu for first time
+print_menu()
+
+#Print menu for first time
+print_menu()
